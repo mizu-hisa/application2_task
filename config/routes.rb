f586818fc231a54f4dba43d4c998b1756e22ca7a
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-04-23 12:41:47 +0000

    [Progress]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+devise_for :users
+root to: 'homes#top'
+get 'homes/about'=>'homes#about', as: 'about'
+get 'books/index'=>'books#index'
+
+
+
+
+resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+resources :users, only: [:show, :edit, :update]
+
+
+
 end
